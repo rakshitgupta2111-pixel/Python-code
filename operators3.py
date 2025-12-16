f1fd8738cfd5f17cfddfde535b6c4e68bac0bd94
rakshitgupta2111-pixel <rakshitgupta2111@gmail.com>
2025-12-16 20:08:35 +0530

    Add files via upload

---
+++ operators3.py
@@ -0,0 +1,10 @@
+print("Enter Marks obtained in 4 subjects:")
+math=int(input("math:"))
+english=int(input("english:"))
+hindi=int(input("hindi:"))
+science=int(input("science:"))
+
+sum=math+english+hindi+science
+perc=(sum/400)*100
+
+print("percentage marks :" , perc)
