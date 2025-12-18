5d554b1ab00fb1f53aaf4e45673de87f51953b66
rakshitgupta2111-pixel <rakshitgupta2111@gmail.com>
2025-12-18 20:11:25 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,12 @@
+current_temperature=int(input("Enter the current temperature :"))
+
+if current_temperature>=20 and current_temperature<=30:
+    print("He can wear light and soft clothes")
+
+elif current_temperature<20:
+    print("He need to wear jackets and pullover")
+     
+else:
+    print("He need to wear very light clothes")
+    
+
