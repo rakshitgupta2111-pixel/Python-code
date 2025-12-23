6735beb57741a47d9c7e697d05c3a43cafb07f42
rakshitgupta2111-pixel <rakshitgupta2111@gmail.com>
2025-12-23 19:46:11 +0530

    Add files via upload

---
+++ logicaloperator2.py
@@ -0,0 +1,20 @@
+a=10
+b=12
+c=12
+
+print(a != b)
+print(b != c)
+
+
+a="python" 
+b="coding"
+
+if a != b:
+    print(a, 'and', b, 'are differnt.')
+
+a=4
+b=5
+
+if (a==1) != (b==5):
+    print("hello")
+    
