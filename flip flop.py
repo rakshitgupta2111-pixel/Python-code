16b7558c49d05264811d4421239e25cc06c9e264
rakshitgupta2111-pixel <rakshitgupta2111@gmail.com>
2026-03-17 19:42:23 +0530

    Add files via upload

---
+++ flip flop.py
@@ -0,0 +1,20 @@
+def palind(r):
+    s=len(r)-1
+    e=0
+
+    while(s<e):
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1 
+    return True
+    
+r=(1,2,3,3,2,1)
+
+if (palind(r)):
+    print("It is flip flop")
+else:
+    print("it is not flip flop")
+
+
+      
